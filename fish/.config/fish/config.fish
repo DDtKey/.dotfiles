cf4256b0962f26436c88135d26e945270d9b6660
Artem Medvedev <i@ddtkey.com>
2024-08-22 18:33:59 +0200

    chore: remove extra fish configs from git

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,20 @@ if status is-interactive
 end
 
 set -g fish_greeting
+
+# init starship
 starship init fish | source
 
+# functions specific for current environment
 if test -f ~/.config/fish/functions/local_fns.fish
     source ~/.config/fish/functions/local_fns.fish
 end
+
+# setup theme
+source ~/.config/fish/theme.fish
+
+# PATH config
+fish_add_path ~/.cargo/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/go/bin
+
