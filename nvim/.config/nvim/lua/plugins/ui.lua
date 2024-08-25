4c3caad33ea3e85e0f68db3e18a8fad60eac603d
Artem Medvedev <i@ddtkey.com>
2024-08-25 16:44:18 +0200

    feat(nvim): change lualine

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -9,4 +9,29 @@ return {
       },
     },
   },
+  {
+      "nvim-lualine/lualine.nvim",
+      opts = {
+          options = {
+              theme = "ayu_mirage",
+              component_separators = "",
+              section_separators = { left = "", right = "" },
+          },
+          sections = {
+              lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+              lualine_y = {
+                  { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                  { "location", padding = { left = 0, right = 1 } },
+              },
+              lualine_z = {
+                  {
+                      --function()
+                      --  --return " " .. os.date("%R")
+                      --end,
+                    "encoding", separator = { right = "" },
+                  },
+              },
+          },
+      },
+  },
 }
