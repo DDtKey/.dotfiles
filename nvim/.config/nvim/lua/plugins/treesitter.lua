2f11f12501779418aa079ca56e12ba9be432df7e
Artem Medvedev <i@ddtkey.com>
2024-08-25 16:43:54 +0200

    chore(nvim): explicitly override treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,26 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- add tsx and treesitter
-      vim.list_extend(opts.ensure_installed, {
-        "rust",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "diff",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
         "ron",
-      })
-    end,
+        "rust",
+        "toml",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
   },
 }
