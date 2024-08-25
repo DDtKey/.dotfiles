22ed874608a9ff16cdbe6af9e5d8d97b9522ddd9
Artem Medvedev <i@ddtkey.com>
2024-08-25 18:36:30 +0200

    feat(nvim): enable cmp-cmdline

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -17,5 +17,35 @@ return {
       opts.sources = opts.sources or {}
       table.insert(opts.sources, { name = "crates" })
     end,
-  }
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+      local cmp = require("cmp")
+      ---@diagnostic disable-next-line: missing-fields
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline({
+          ["<C-j>"] = {
+            c = function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              else
+                fallback()
+              end
+            end,
+          },
+        }),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
 }
