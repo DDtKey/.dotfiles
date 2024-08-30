3d6df311320beaf256ce908e04245fe7a6edc072
Artem Medvedev <i@ddtkey.com>
2024-08-30 20:33:40 +0200

    feat(nvim): add alpha-nvim config

---
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -0,0 +1,118 @@
+
+local logos = {
+  [[
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡠⠤⠒⠈⠉⠉⠉⠉⠉⠉⠐⠢⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⣠⠖⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⡠⠞⡁⠀⠀⠀⠀⣀⣠⢠⣀⣀⣀⣤⣠⣀⡀⠀⠀⢠⣀⠑⡄⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⣰⠃⠀⣷⣶⡀⠀⣹⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⢿⣶⣿⣿⣤⠘⡆⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢰⠇⠀⢠⡹⣿⣿⣿⣿⡿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠑⠛⠦⣄⡀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⢸⠀⠀⢠⣽⡿⠟⠋⠠⠂⣀⣤⣶⣾⣿⣿⣿⣿⡿⢿⣶⣦⣄⢀⣄⡀⠀⣤⡍⠓⢦⡀⠀
+  ⠀⠀⠀⠀⢸⠀⡨⢞⣁⣴⣶⣶⣶⣿⣿⣿⡿⠃⡿⢻⣿⡿⠁⠈⢻⡍⢿⣿⣝⡻⢿⣅⠀⠀⠀⠙⡄
+  ⠀⠀⠀⠀⣨⠟⠁⢺⡿⢋⣾⣿⣿⣿⣿⡿⠁⢰⠃⢸⡿⠁⠀⠀⠸⡇⠀⢿⣿⣿⣦⣉⠀⠀⠀⠀⢸
+  ⠀⠀⣠⠞⠁⠀⠀⠀⣠⣿⣿⣿⡿⢹⣿⠁⠀⠈⢀⡞⢀⣀⡀⠀⠀⠁⠀⠘⣿⣿⣅⣤⣄⠀⠀⢀⡞
+  ⠀⢰⠃⠀⣠⣄⠀⢰⣿⣿⣿⣿⠃⢸⠷⣤⣀⣀⣈⣡⠟⢇⣁⣀⣤⣂⡀⠀⢻⣿⣿⣿⣿⠀⣠⠎⠀
+  ⢠⡇⠀⢰⣿⣟⠀⣾⣿⣿⣿⣿⠀⠺⠿⠿⠬⠛⠿⠋⠀⠞⠙⣿⣿⣿⡋⠀⣾⣿⣤⣿⡡⠞⠁⠀⠀
+  ⠈⡇⠀⢸⣿⣿⣳⢿⣿⣿⢿⡟⠀⠀⠀⠀⢀⡀⠰⠦⠀⢸⠿⠢⢍⡉⠁⢰⣳⡯⠛⠉⠀⠀⠀⠀⠀
+  ⠀⠱⡀⢼⣿⣿⣯⠼⢿⡹⢯⣿⠀⠀⠀⠔⠉⠀⢀⣀⣀⣀⡀⠀⠀⠱⠀⢸⠏⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠳⣌⡛⠛⣧⣤⣼⣏⣙⣾⣇⠀⠀⢀⠴⠊⢁⡤⡤⣤⣈⠑⠲⢄⠀⡎⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠉⠉⠁⠉⠀⠀⠀⠀⠈⠳⣤⣄⡤⠊⠁⠘⠀⠇⠛⢉⣲⠞⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⣽⣿⣶⣾⣿⢿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⠀⠀⠈⠙⣿⣿⡼⣧⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣶⣿⣿⣧⠀⠀⠀⠀⠈⠉⠁⠈⢙⣿⣷⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⢴⣾⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀
+  ⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀
+  ]],
+  [[
+  ⠀⠀⡶⠛⠲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡶⠚⢶⡀⠀
+  ⢰⠛⠃⠀⢠⣏⠀⠀⠀⠀⣀⣠⣤⣤⣤⣤⣤⣤⣄⣀⡀⠀⠀⠀⣸⠇⠀⠈⠙⣧
+  ⠸⣦⣤⣄⠀⠙⢷⣤⣶⠟⠛⢉⣁⣤⣤⣤⣤⣀⣉⠙⠻⢷⣤⡾⠋⢀⣤⣤⣴⠏
+  ⠀⠀⠀⠈⠳⣤⡾⠋⣀⣴⣿⣿⠿⠿⠟⠛⠿⠿⣿⣿⣶⣄⠙⢿⣦⠟⠁⠀⠀⠀
+  ⠀⠀⠀⢀⣾⠟⢀⣾⣿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣷⡄⠹⣷⡀⠀⠀⠀
+  ⠀⠀⠀⣾⡏⢠⣿⣿⡯⠤⠤⠤⠒⠒⠒⠒⠒⠒⠒⠤⠤⠽⣿⣿⡆⠹⣷⡀⠀⠀
+  ⠀⠀⢸⣟⣠⡿⠿⠟⠒⣒⣒⣉⣉⣉⣉⣉⣉⣉⣉⣉⣒⣒⡛⠻⠿⢤⣹⣇⠀⠀
+  ⠀⠀⣾⡭⢤⣤⣠⡞⠉⠁⢀⣀⣀⠀⠀⠀⠀⢀⣀⣀⠀⠈⢹⣦⣤⡤⠴⣿⠀⠀
+  ⠀⠀⣿⡇⢸⣿⣿⣇⠀⣼⣿⣿⣿⣷⠀⠀⣼⣿⣿⣿⣷⠀⢸⣿⣿⡇⠀⣿⠀⠀
+  ⠀⠀⢻⡇⠸⣿⣿⣿⡄⢿⣿⣿⣿⡿⠀⠀⢿⣿⣿⣿⡿⢀⣿⣿⣿⡇⢸⣿⠀⠀
+  ⠀⠀⠸⣿⡀⢿⣿⣿⣿⣆⠉⠛⠋⠀⢴⣶⠀⠉⠛⠉⣠⣿⣿⣿⡿⠀⣾⠇⠀⠀
+  ⠀⠀⠀⢻⣷⡈⢻⣿⣿⣿⣿⣶⣤⣀⣈⣁⣀⡤⣴⣿⣿⣿⣿⡿⠁⣼⠏⠀⠀⠀
+  ⠀⠀⠀⢀⣽⣷⣄⠙⢿⣿⣿⡟⢲⠧⡦⠼⠤⢷⢺⣿⣿⡿⠋⣠⣾⢿⣄⠀⠀⠀
+  ⣰⠟⠛⠛⠁⣨⡿⢷⣤⣈⠙⢿⡙⠒⠓⠒⠒⠚⡹⠛⢁⣤⣾⠿⣧⡀⠙⠋⠙⣆
+  ⠹⣤⡀⠀⠐⡏⠀⠀⠉⠛⠿⣶⣿⣶⣤⣤⣤⣾⣷⠾⠟⠋⠀⠀⢸⡇⠀⢠⣤⠟
+  ⠀⠀⠳⢤⠾⠃⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠘⠷⠤⠾⠁⠀
+  ]],
+}
+
+
+local function pick_color()
+  local colors = { "String", "Identifier", "Keyword", "Number" }
+  math.randomseed(os.time())
+  return colors[math.random(#colors)]
+end
+
+return {
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    enabled = true,
+    init = false,
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+
+      math.randomseed(os.time())
+      local logo = logos[math.random(#logos)]
+      dashboard.section.header.val = vim.split(logo, "\n")
+
+      -- stylua: ignore
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file",       LazyVim.pick()),
+        dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+        dashboard.button("r", " " .. " Recent files",    LazyVim.pick("oldfiles")),
+        dashboard.button("g", " " .. " Find text",       LazyVim.pick("live_grep")),
+        dashboard.button("c", " " .. " Config",          LazyVim.pick.config_files()),
+        dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+      }
+      for i, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = pick_color()
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          once = true,
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+            .. stats.loaded
+            .. "/"
+            .. stats.count
+            .. " plugins in "
+            .. ms
+            .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
+        end,
+      })
+    end,
+  }
+}
