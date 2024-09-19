4b38d3d9d86f96abb359369494199cdf444eaa2a
Artem Medvedev <i@ddtkey.com>
2024-09-20 01:18:25 +0200

    chore(nvim): disable autocmd for :wall

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,15 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-  pattern = { "*" },
-  command = "silent! wall",
-  nested = true,
-})
+
+-- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+--   pattern = { "*" },
+--   command = "silent! wall",
+--   nested = true,
+-- })
 
 -- Autoformat settings
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*.rs" },
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "rust" },
   callback = function()
     vim.b.autoformat = true
   end,
