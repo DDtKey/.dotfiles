ff8ec94b26605e7cf71bd0abee80df06ca52eb33
Artem Medvedev <i@ddtkey.com>
2024-11-06 16:37:16 +0800

    chore(nvim): update not to use depricated calls

--- nvim/.config/nvim/lua/plugins/silicon.lua
+++ nvim/.config/nvim/lua/plugins/silicon.lua
@@ -5,9 +5,9 @@ return {
     cmd = "Silicon",
     init = function()
       local wk = require("which-key")
-      wk.register({
-        ["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
-      }, { mode = "v" })
+      wk.add({
+        { "<leader>sc", ":Silicon<CR>", desc = "Snapshot Code", mode = "v" },
+      })
     end,
     config = function(_)
       require("silicon").setup({
